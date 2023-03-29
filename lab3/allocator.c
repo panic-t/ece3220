dedff65ce03cdcaf9104a3ef631125ff50743ec8
panic-t <ryanjinx7@gmail.com>
2023-03-29 09:52:40 -0400

    got it

---
+++ lab3/allocator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PAGESIZE 4096
+
+//allocates size bytes from memory, returns pointer to memory
+void *malloc(size_t size) {
+    
+}
+
+//allocates size*n cleared bytes from memory, returns pointer to memory
+void *calloc(size_t n, size_t size) {
+
+}
+
+//reallocates memory at ptr to size bytes, returns new ptr
+void *realloc(void *ptr, size_t size) {
+
+}
+
+//returns ptr to unallocated memory
+void free(void *ptr) {
+
+}
