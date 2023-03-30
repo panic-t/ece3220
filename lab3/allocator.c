fe86ea8db35d2061e8cfb654ce36cae570dd4d07
panic-t <ryanjinx7@gmail.com>
2023-03-30 12:47:14 -0400

    work done by mar 30

--- lab3/allocator.c
+++ lab3/allocator.c
@@ -1,24 +1,43 @@
+/* <ryan ware>
+ECE 3220 S23
+project 3
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
-#define PAGESIZE 4096
+
+//header block
+//XXXX
+
+//USE BUILTIN CLZ
 
 //allocates size bytes from memory, returns pointer to memory
 void *malloc(size_t size) {
-    
+    if(size<1) {
+        return NULL;
+    }
+
 }
 
 //allocates size*n cleared bytes from memory, returns pointer to memory
 void *calloc(size_t n, size_t size) {
-
+    if(size*n<1) {
+        return NULL;
+    }
 }
 
 //reallocates memory at ptr to size bytes, returns new ptr
 void *realloc(void *ptr, size_t size) {
-
+    if(ptr==NULL) {
+        return NULL;
+    }
+    //what to do when size<1
 }
 
 //returns ptr to unallocated memory
 void free(void *ptr) {
-
+    if(ptr==NULL) {
+        return;
+    }
 }
